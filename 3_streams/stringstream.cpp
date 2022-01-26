5ed1fdb9bb5455994813db6745995f16c2dfd52b
trrbivial <shenji233@gmail.com>
2022-01-26 16:27:11 +0800

    add source_code

--- 3_streams/stringstream.cpp
+++ 3_streams/stringstream.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 #include<sstream>
 using namespace std;
+int stringTointeger(const string& str){
+    istringstream iss(str);
+    int res;
+    iss >> res;
+    if (iss.fail()) throw domain_error("114514");
+    char tmp;
+    iss >> tmp;
+    if (!iss.fail()) throw domain_error("1919810");
+    return res;
+}
 int main(){
 	ostringstream oss("Ito En Green Tea");
-	cout<< oss.str() << '\n';
+    cout << oss.str() << '\n';
+
+    string s;
+    cin >> s;
+    int res = stringTointeger(s);
+    cout << res << '\n';
 	return 0;
 }
