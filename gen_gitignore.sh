dda37d29f0a83c069e56cd2c3eee9cb8c7dc7053
trrbivial <shenji233@gmail.com>
2022-01-26 17:22:40 +0800

    simplified gen_gitignore.sh

--- gen_gitignore.sh
+++ gen_gitignore.sh
@@ -3,35 +3,27 @@
     cat ~/gitignore_files/C++.gitignore
 } > .gitignore
 
-find -name "*.cpp" > findfile.tmp
-
 {
     echo "#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    freopen(\"findfile.tmp\",\"r\",stdin);
-    freopen(\"findfile.ttmp\",\"w\",stdout);
     string s;
     while(cin>>s){
         string t=\"\";
-        for(int i=2;i+4<s.length();i++)t+=s[i];
+        for(size_t i=2;i+4<s.length();i++)t+=s[i];
         cout<<t<<'\n';
     }
     return 0;
 }"
 } > gen_gitignore.cpp
 
-g++ -std=c++17 gen_gitignore.cpp -o gen_gitignore
-
-./gen_gitignore
-
+g++ -std=c++17 -Wall gen_gitignore.cpp -o gen_gitignore
 {
     echo ''
     echo '# *.cpp file'
-    cat findfile.ttmp
+    find -name "*.cpp" | ./gen_gitignore
 } >> .gitignore
 
-rm -f findfile.tmp findfile.ttmp
 rm -f gen_gitignore.cpp gen_gitignore
 
 {
