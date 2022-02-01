72b50561c339efea57242ffb3252640149469d6b
trrbivial <shenji233@gmail.com>
2022-02-01 20:41:18 +0800

    add course code

---
+++ 7_template_classes/cci.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int b[1005];
+using miterator = int*;
+using const_iterator = const int*;
+int main(){
+    miterator a1 = b;
+    const const_iterator a2 = b;
+    return 0;
+}
